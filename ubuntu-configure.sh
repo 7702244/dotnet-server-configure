385bdcea40da8e5dd86afdb048e6ad3eeb6a82f5
7702244 <56172899+7702244@users.noreply.github.com>
2023-02-16 18:43:09 +0200

    Update DH group generation

--- ubuntu-configure.sh
+++ ubuntu-configure.sh
@@ -72,7 +72,7 @@ sudo systemctl status webmin
 # Create Diffie-Hellman (DH) group
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-in-ubuntu-20-04-1
 echo -e "${cyan:-}Creating Diffie-Hellman (DH) group${normal:-}"
-sudo openssl dhparam -out /etc/nginx/dhparam.pem 4096
+sudo openssl dhparam -dsaparam -out /etc/nginx/dhparam.pem 2048
 
 # Install .NET
 # https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
